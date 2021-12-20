59d56274636590cc3c3b23c1775d6fec2f745f73
Mohsn <mohsnmohsni.co@gmail.com>
2021-12-20 09:18:34 +0330

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,9 @@
+# while ! nc -z db 3306 ; do
+#     echo "Waiting for the MySQL Server"
+#     sleep 3
+# done
+
+sleep 10
+
 python manage.py migrate --no-input
 python manage.py runserver 0.0.0.0:8000
