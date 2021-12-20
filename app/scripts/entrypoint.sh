41066a59aa0dea02c5765c5f933e2cf253f0af72
Mohsn <mohsnmohsni.co@gmail.com>
2021-12-20 15:25:51 +0330

    add bashscript while to get error

--- app/scripts/entrypoint.sh
+++ app/scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+while ! python manage.py sqlflush > /dev/null 2>&1; do echo "sleep" && sleep 1; done;
+
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py runserver 0.0.0.0:8000
