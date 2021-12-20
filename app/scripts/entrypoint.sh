dd1e875b5b083286b61805a16cdfb1ca95dbaedf
Mohsn <mohsnmohsni.co@gmail.com>
2021-12-20 15:12:40 +0330

    add py commands to entrypoint

--- app/scripts/entrypoint.sh
+++ app/scripts/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# while ! nc -z db 3306 ; do
-#     echo "Waiting for the MySQL Server"
-#     sleep 3
-# done
-
-echo "hello"
-
-# python manage.py makemigrations --no-input
-# python manage.py migrate --no-input
-# python manage.py runserver 0.0.0.0:8000
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py runserver 0.0.0.0:8000
